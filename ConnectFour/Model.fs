45dc8a55b7c41a415a0697e567eef9c33c54ba41
Mikhail Brinchuk <mikhail.brinchuk@prodrive-technologies.com>
2022-05-21 10:25:17 +0200

    Model WIP

---
+++ ConnectFour/Model.fs
@@ -0,0 +1,53 @@
+﻿module ConnectFour.Model
+
+// todo mikbri use reader monad to provide these constants?
+
+let rowsNum = 6
+let colsNum = 7
+let win = 5
+let depth = 6
+
+type Player =
+    | O
+    | Blank
+    | X
+    
+type Row = List<Player>
+
+type Board = List<Row>
+
+let rows board = board
+
+let columns (board : Board) = List.transpose board
+
+let diagonals (board : Board) =
+    let shiftedRows (shift : int) (rowMultiplier : int) =
+        board
+        |> List.mapi(fun rowIndex row ->
+            row |> List.safeSkip (shift + rowIndex * rowMultiplier))
+        |> List.filter (not << List.isEmpty)
+
+    [
+        for multiplier in [1; -1] do
+            for shift in [0 .. colsNum + rowsNum - 1] do
+                shiftedRows shift multiplier |> List.transpose
+    ]
+    |> List.concat
+    
+let winnerInRow (row : Row) =
+    let rec impl (prev, count) cells =
+        match cells with
+        | [] -> None
+        | Blank :: rest -> impl (Blank, 1) rest
+        | player :: _ when player = prev && count + 1 >= win -> Some player
+        | player :: rest when player = prev -> impl (player, count + 1) rest
+        | player :: rest -> impl (player, 1) rest
+    
+    impl (Blank, 0) row
+
+let winner (board : Board) =
+    rows board @ columns board @ diagonals board
+    |> List.choose winnerInRow
+    |> List.tryHead
+
+let nextMoves (player : Player) (board : Board) = []
---
+++ ConnectFour/Prelude.fs
@@ -0,0 +1,16 @@
+﻿[<AutoOpen>]
+module ConnectFour.Prelude
+
+module List =
+
+    let safeSkip count list =
+        if count <= 0 then
+            list
+        else
+            let rec loop i lst =
+                match lst with
+                | _ when i = 0 -> lst
+                | _ :: t -> loop (i - 1) t
+                | [] -> []
+
+            loop count list
